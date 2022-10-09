41bc7c48f0b9cc547fe67f91f0f949df9b588e1b
ParkHyunKyeong <kj3477417@gmail.com>
2022-10-09 18:28:14 +0900

    feature: add HashRouter, Routes - install react-router-dom, @types/react-router-dom - go 'Home' when connecting unsupported URLs - available url: '/', '/about' - 이슈: '/about'을 입력해서 홈 화면이 보이는 버그가 있음. 이후 업데이트에서 수정 예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home'
+import About from './routes/About'
 
 const App: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    const getMovies = async (): Promise<void> => {
-      const {
-        data: {
-          data: {
-            movies
-          }
-        }
-      } = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating')
-      setMovies(movies);
-      setIsLoading(false);
-    }
-    getMovies()
-  }, [])
   return (
-    <section className="container">
-      { 
-        isLoading ? (
-          <div className="loader">
-            <span>Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {
-              movies.map((movie: any) => (
-                <Movie
-                  key={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              ))
-            }
-          </div>
-        )
-      };
-    </section>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='*' element={<Home/>} />
+      </Routes>
+    </HashRouter>
   );
 }
 
---
+++ src/routes/Home.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
+
+const Home = (): React.ReactElement => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async (): Promise<void> => {
+      const {
+        data: {
+          data: {
+            movies
+          }
+        }
+      } = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating')
+      setMovies(movies);
+      setIsLoading(false);
+    }
+    getMovies()
+  }, [])
+
+  return (
+    <section className="container">
+      { 
+        isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {
+              movies.map((movie: any) => (
+                <Movie
+                  key={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))
+            }
+          </div>
+        )
+      };
+    </section>
+  );
+}
+
+export default Home;
