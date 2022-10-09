f15d8310eca9e5cc11be355865a96b5f6d1937e3
ParkHyunKyeong <kj3477417@gmail.com>
2022-10-09 17:28:29 +0900

    feature: add genres to Movie Component

--- src/Movie.tsx
+++ src/Movie.tsx
@@ -3,15 +3,24 @@ export interface MovieType {
     title: string,
     summary: string,
     poster: string,
+    genres: string[],
 }
 
-const Movie = ({year, title, summary, poster}: MovieType) => {
+const Movie = ({year, title, summary, poster, genres}: MovieType) => {
     return (
         <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {
+                        genres.map((genre: string, index: number) => (
+                            <li key={index} className="genres__genre">{genre}</li>
+                        ))
+                    }
+
+                </ul>
                 <p className="movie__summary">{summary}</p>
             </div>
         </div>
