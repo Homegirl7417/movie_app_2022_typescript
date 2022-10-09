f1b2a63868efd887e21ad351034e8069d634ff92
ParkHyunKyeong <kj3477417@gmail.com>
2022-10-09 17:55:23 +0900

    refactor: change movie genres className and apply Movie.css

--- src/Movie.tsx
+++ src/Movie.tsx
@@ -1,4 +1,6 @@
-export interface MovieType {
+import './Movie.css'
+
+interface MovieType {
     year: number,
     title: string,
     summary: string,
@@ -13,7 +15,7 @@ const Movie = ({year, title, summary, poster, genres}: MovieType) => {
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <ul className="genres">
+                <ul className="movie__genres">
                     {
                         genres.map((genre: string, index: number) => (
                             <li key={index} className="genres__genre">{genre}</li>
